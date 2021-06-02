2a35eb61fc29304d1e2edd54ba72cb5ed5a65fa5
1RandomDev <84292805+1RandomDev@users.noreply.github.com>
2021-06-03 00:53:27 +0200

    Fixed connection issues

--- index.js
+++ index.js
@@ -21,54 +21,40 @@ mqttClient.on('error', err => {
     process.exit(1);
 });
 
-mqttClient.publish('switchbot/available', 'online', {retain: true});
+mqttClient.on('connect', async () => {
+    console.log('Connected to mqtt broker');
+    mqttClient.publish('switchbot/available', 'online', {retain: true});
+});
+
 
 (async () => {
     const devices = config.devices;
     await asyncForEach(devices, async device => {
-        device.nextBatteryMessage = 0;
         device.id = device.address.replace(/:/g, '').toLowerCase();
 
-        const clients = await switchbot.discover({ id: device.address, quick: true });
-        if(clients.length == 0) {
-            console.log('Connection to device '+device.name+' failed.');
-        } else {
-            device.client = clients[0];
-            advertiseHomeAssistantDevice(device);
-        }
+        advertiseHomeAssistantDevice(device);
     });
-
-    await switchbot.startScan();
-    switchbot.onadvertisement = ad => {
-        const device = devices.find(device => device.address == ad.address);
-        const currentTime = Date.now()
-
-        if(device.nextBatteryMessage < currentTime) {
-            device.nextBatteryMessage = currentTime + 600000; // = 10min
-            mqttClient.publish('switchbot/'+device.id+'/battery', ad.serviceData.battery.toString(), {retain: true});
-        }
-
-        if(device.invertState) ad.serviceData.state = !ad.serviceData.state;
-        if(device.lastState == null || device.lastState != ad.serviceData.state) {
-            device.lastState = ad.serviceData.state;
-            mqttClient.publish('switchbot/'+device.id+'/state', ad.serviceData.state ? 'ON' : "OFF", {retain: true});
-        }
-    };
 
     mqttClient.subscribe('switchbot/+/control');
     mqttClient.on('message', async (topic, payload) => {
         const currentId = topic.match(/switchbot\/(.+)\/control/)[1];
         const device = devices.find(device => device.id == currentId);
-
-        switchbot.stopScan();
-
-        if(payload == 'ON') {
-            await device.client.turnOn()
-        } else if(payload == 'OFF') {
-            await device.client.turnOff();
+        if(device == null) {
+            console.error('Device with id '+currentId+' not found!');
+            return;
         }
 
-        switchbot.startScan();
+        const clients = await switchbot.discover({ id: device.address, quick: true });
+        if(clients.length == 0) {
+            console.log('Connection to device '+device.name+' failed.');
+        } else {
+           const client = clients[0];
+            if(payload == 'ON') {
+                await client.turnOn()
+            } else if(payload == 'OFF') {
+                await client.turnOff();
+            }
+        }
     });
 })();
 
@@ -84,9 +70,9 @@ function advertiseHomeAssistantDevice(device) {
         name: device.name,
         unique_id: 'sb_'+device.id+'_switch',
         availability_topic: 'switchbot/available',
-        state_topic: 'switchbot/'+device.id+'/state',
         command_topic: 'switchbot/'+device.id+'/control',
-        device: deviceInfo
+        device: deviceInfo,
+        optimistic: true
     }));
 
     mqttClient.publish('homeassistant/sensor/sb_'+device.id+'_battery/config', JSON.stringify({
@@ -99,6 +85,30 @@ function advertiseHomeAssistantDevice(device) {
         device_class: 'battery',
         device: deviceInfo
     }));
+}
+
+async function getDeviceInfo() {
+    devices.forEach(device => {
+        device.scanComplete = false;
+    });
+
+    switchbot.onadvertisement = ad => {
+        const device = devices.find(device => device.address == ad.address);
+        if(device == null) return;
+
+        if(device.scanComplete) return;
+        device.scanComplete = true;
+
+        mqttClient.publish('switchbot/'+device.id+'/battery', ad.serviceData.battery.toString(), {retain: true});
+    };
+
+    switchbot.startScan();
+    await delay(5000);
+    switchbot.stopScan();
+}
+
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function asyncForEach(array, callback) {
